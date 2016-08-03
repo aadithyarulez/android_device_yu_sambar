6d5db0e4f7e61d0ee593129c29dfd056c27cd6b6
HostZero <haneesh.da.rocker@gmail.com>
2016-08-03 07:05:04 +0530

    sambar: targeting dtb
    Change-Id: I39db028dfb8376c8cd034434717fc431ad66e7e0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -56,13 +56,14 @@ BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 boot_cpus=0-5
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DT := true
-BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x01E00000 --dt device/yu/sambar/prebuilt/dt.img
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x01E00000 
 ifneq ($(SAMBAR_32_BIT),true)
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 endif
 TARGET_PREBUILT_KERNEL := device/yu/sambar/prebuilt/kernel
+TARGET_PREBUILT_DTB := device/yu/sambar/prebuilt/dt.img
 
 # CM Hardware
 BOARD_USES_CYANOGEN_HARDWARE := true
